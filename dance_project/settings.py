88d05692107c98442abc779ea7b02765fd47dfa9
EC2 Default User <ec2-user@ip-172-31-36-206.eu-west-1.compute.internal>
2024-04-21 16:29:00 +0000

    pipelie6

--- dance_project/settings.py
+++ dance_project/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = (
     BASE_DIR / "static",
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
